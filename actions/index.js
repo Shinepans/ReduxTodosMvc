3fabdd4731ece1ca493b36f5c1a3b3024f6fbfde
Shinepans <shinepans@live.com>
2016-01-31 18:33:20 +0800

    noti

--- actions/index.js
+++ actions/index.js
@@ -1,25 +1,31 @@
 import * as types from '../constants/ActionTypes'
 
+//添加
 export function addTodo(text) {
   return { type: types.ADD_TODO, text }
 }
 
+//删除
 export function deleteTodo(id) {
   return { type: types.DELETE_TODO, id }
 }
 
+//编辑
 export function editTodo(id, text) {
   return { type: types.EDIT_TODO, id, text }
 }
 
+//完成
 export function completeTodo(id) {
   return { type: types.COMPLETE_TODO, id }
 }
 
+//全部完成
 export function completeAll() {
   return { type: types.COMPLETE_ALL }
 }
 
+//清楚完成
 export function clearCompleted() {
   return { type: types.CLEAR_COMPLETED }
 }
--- components/Footer.js
+++ components/Footer.js
@@ -8,11 +8,13 @@ const FILTER_TITLES = {
   [SHOW_COMPLETED]: 'Completed'
 }
 
+//页脚
 class Footer extends Component {
   renderTodoCount() {
     const { activeCount } = this.props
+    //控制item单词显示单数或复数
     const itemWord = activeCount === 1 ? 'item' : 'items'
-
+    //如果没有todo, 则显示no items left
     return (
       <span className="todo-count">
         <strong>{activeCount || 'No'}</strong> {itemWord} left
@@ -20,10 +22,12 @@ class Footer extends Component {
     )
   }
 
+//过滤器
   renderFilterLink(filter) {
     const title = FILTER_TITLES[filter]
+    //有三种属性: 全部, 正在执行, 完成
     const { filter: selectedFilter, onShow } = this.props
-
+    //根据属性显示相对应的集合
     return (
       <a className={classnames({ selected: filter === selectedFilter })}
          style={{ cursor: 'pointer' }}
